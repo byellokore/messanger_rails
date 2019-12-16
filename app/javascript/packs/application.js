bd1791b0a65e472b5105c404197acf5a8517ce6d
gabriel fontoura dos santos <gabrielfontoura@me.com>
2019-12-15 21:32:18 -0500

    font-awesome moved from asset to webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+import "@fortawesome/fontawesome-free/css/all.css"
 
 import bulmaQuickview from "bulma-quickview/src/js"
 
